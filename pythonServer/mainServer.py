813bb5e1f5fff976da20a803992ba8a9f743ca80
cameron <ckiw23@gmail.com>
2024-02-03 18:55:13 -0500

    added function to allign words to support model inference

--- pythonServer/mainServer.py
+++ pythonServer/mainServer.py
@@ -1,8 +1,10 @@
 import flask
+import numpy
 from flask import request, make_response
 import numpy as np
 import joblib
 import lightbeam
+import sklearn
 
 app = flask.Flask(__name__)
 
@@ -20,14 +22,36 @@ def processProduct():
             "description": f"{data['description']}",
             "asin": f"{data['asin']}"
         }
+
+        inferFromModel(productName=productInfo.get("name"))
         response = make_response(productInfo, 200)  # wait for josh to do NLP bullshit
         return response
 
 
-def inferFromModel():
+def inferFromModel(productName: str):
     rf = joblib.load("../Data/lgbm_classifier_model.joblib")
-    np.load(../)
+    validWords = np.load("../Data/col_names.npy")
+    words = np.char.split(productName, " ")
+
+    mask = np.isin(words, validWords)
+    words = words[mask]
+
+    np.sort(words)
+
+    wordFixer = np.vectorize(align_words)
+    words = wordFixer(words, validWords)
 
 
+print(np.load("../Data/col_names.npy", allow_pickle=True))
 
 
+def align_words(words, valid_words):
+    aligned_words = []
+    for word in valid_words:
+        if word in words:
+            aligned_words.append(word)
+        else:
+            aligned_words.append(0)
+    return aligned_words
+
+# Example usage:
