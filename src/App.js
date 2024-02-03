db63592c48411bbc35d23c096c96a47941e568d7
MollyShe <mollys@vt.edu>
2024-02-03 15:30:56 -0500

    better left and right buttons

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import { useState } from 'react';
 function App() {
   // red button stuff
   const handleClick = () => {
-    setIsRed(!isRed);
+    setIsPink(!isPink);
   };
-  const [isRed, setIsRed] = useState(false);
-  // end red button stuff
+  const [isPink, setIsPink] = useState(false);
 
   // ok let's do option choosing
   const [currentOption, setCurrentOption] = useState(0);
   const options = ['product 1', 'product 2', 'product 3'];
 
+  // left and right moving
   const handleLeft = () => {
     setCurrentOption(prev => (prev -1 + options.length) % options.length);
   };
@@ -37,7 +37,7 @@ function App() {
         >
           Learn React
         </a>
-        <button onClick={handleClick} className="App-button" style={{backgroundColor: isRed ? 'red' : 'initial'}}>
+        <button onClick={handleClick} className="App-button" style={{backgroundColor: isPink ? '#bd4880' : 'initial'}}>
           Click Me
         </button>
 
@@ -45,8 +45,8 @@ function App() {
         <div>{options[currentOption]}</div>
         {/* Navigation buttons */}
         <div className="navigation">
-          <button onClick={handleLeft}>&lt; Left</button>
-          <button onClick={handleRight}>Right &gt;</button>
+          <button onClick={handleLeft} className="left-triangle"></button>
+          <button onClick={handleRight} className="right-triangle"></button>
         </div>
       </header>
     </div>
