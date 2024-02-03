3e570098a45d476c4077926c0392ef90b4691c43
MollyShe <mollys@vt.edu>
2024-02-03 13:34:52 -0500

    added smaller logos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const handleClick = () => {
+    setIsRed(!isRed);
+  };
+
+  const [isRed, setIsRed] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and yayyyy to reload.
+          Edit <code>src/App.js</code> and fun away yayyyy to reload.
         </p>
         <a
           className="App-link"
@@ -17,6 +24,9 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={handleClick} className="App-button" style={{backgroundColor: isRed ? 'red' : 'initial'}}>
+          Click Me
+        </button>
       </header>
     </div>
   );
