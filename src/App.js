82d34180a29bb27b43267ef0edb87c8307ee582e
cameron <ckiw23@gmail.com>
2024-02-03 16:01:47 -0500

    added smile placeholder images

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+
+  var count = 0;
   // red button stuff
   const handleClick = () => {
     setIsRed(!isRed);
@@ -14,21 +16,30 @@ function App() {
   const [currentOption, setCurrentOption] = useState(0);
   const options = ['product 1', 'product 2', 'product 3'];
 
+  const updateImage = function(count) {
+    document.getElementById('product image').src = "smile" + (count % options.length) + ".jpg"
+  }
+
   const handleLeft = () => {
-    setCurrentOption(prev => (prev -1 + options.length) % options.length);
+    count = (count -1 + options.length)
+    setCurrentOption(count);
+
   };
 
   const handleRight = () => {
-    setCurrentOption(prev => (prev + 1) % options.length);
+    count = (count + 1) % options.length
+    setCurrentOption(count);
+    updateImage(count);
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and fun away yayyyy to reload.
-        </p>
+        <div className='image'>
+          <img id='product image' src='' />
+        </div>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -37,6 +48,7 @@ function App() {
         >
           Learn React
         </a>
+       
         <button onClick={handleClick} className="App-button" style={{backgroundColor: isRed ? 'red' : 'initial'}}>
           Click Me
         </button>
@@ -44,10 +56,11 @@ function App() {
         {/* Render the current option*/}
         <div>{options[currentOption]}</div>
         {/* Navigation buttons */}
-        <div className="navigation">
+        <div className="navigation" >
           <button onClick={handleLeft}>&lt; Left</button>
           <button onClick={handleRight}>Right &gt;</button>
         </div>
+
       </header>
     </div>
   );
