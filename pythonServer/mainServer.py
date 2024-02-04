c1a971028f4d196ad59827a01c54f9b376790e67
cameron <ckiw23@gmail.com>
2024-02-03 22:04:51 -0500

    working on the parser

--- pythonServer/mainServer.py
+++ pythonServer/mainServer.py
@@ -1,11 +1,16 @@
+import json
+
 import flask
 import numpy
-from flask import request, make_response
+from flask import request, make_response, jsonify
 import numpy as np
 import joblib
 import lightbeam
 import sklearn
-import pandas
+import re
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+import joblib  # for saving and loading sklearn models
 
 app = flask.Flask(__name__)
 
@@ -17,11 +22,13 @@ def processProduct():
         productInfo = {
             "name": f"{data['productName']}",
             "units": f"{data['units']}",
-            "price": f"{data['price']}",
+            "priceWhole": f"{data['priceWhole']}",
+            "priceFraction": f"{data['priceFraction']}",
             "numRatings": f"{data['numRatings']}",
             "numStars": f"{data['numStars']}",
             "description": f"{data['description']}",
-            "asin": f"{data['asin']}"
+            "asin": f"{data['asin']}",
+            "infoDump": f"{data['infoDump']}"
         }
 
         inferFromModel(productName=productInfo.get("name"))
@@ -41,12 +48,9 @@ def inferFromModel(productName: str):
 
     wordFixer = np.vectorize(align_words)
     words = wordFixer(words, validWords)
-    df = pandas.DataFrame(words, columns=validWords)
+    df = pd.DataFrame(words, columns=validWords)
 
     rf.predict_proba(df)
-
-
-print(np.load("../Data/col_names.npy", allow_pickle=True))
 
 
 def align_words(words, valid_words):
@@ -58,4 +62,135 @@ def align_words(words, valid_words):
             aligned_words.append(0)
     return aligned_words
 
+
 # Example usage:
+def sanatizeInfoDump(infoDump: str):
+    infoDump = re.sub("[ ]{2,}", repl=" ", string=infoDump)
+    infoDump = re.sub(".u200.", repl="", string=infoDump)
+    # infoDump = re.sub("\\n", repl="", string=infoDump)
+    print(infoDump)
+    print(infoDump[172])
+    return json.loads(infoDump)
+
+
+def getImportantDetails(productJSON):
+    listProducts = productJSON["listProducts"]
+    manufacturterIndex = listProducts.find("Manufacturer")
+    if manufacturterIndex != -1:
+        listProducts[manufacturterIndex]
+
+    # Split the input string by the delimiter "‏ : ‎"
+    segments = listProducts.split("‏ : ‎")
+
+    # Initialize variables to store extracted data
+    product_dimensions = "Not Available"
+    item_model_number = "Not Available"
+    date_first_available = "Not Available"
+    manufacturer = "Not Available"
+    asin = "Not Available"
+    country_of_origin = "Not Available"
+
+    print(segments)
+    # Iterate over the segments and extract data
+    for segment in segments:
+        # Clean up the segment
+        segment = segment.strip()
+
+        # Check if the segment contains any relevant data
+        if segment.startswith("Product Dimensions"):
+            product_dimensions = segment[len("Product Dimensions"):].strip()
+        elif segment.startswith("Item model number"):
+            item_model_number = segment[len("Item model number"):].strip()
+        elif segment.startswith("Date First Available"):
+            date_first_available = segment[len("Date First Available"):].strip()
+        elif segment.startswith("Manufacturer"):
+            manufacturer = segment[len("Manufacturer"):].strip()
+        elif segment.startswith("ASIN"):
+            asin = segment[len("ASIN"):].strip()
+        elif segment.startswith("Country of Origin"):
+            country_of_origin = segment[len("Country of Origin"):].strip()
+    print(product_dimensions, item_model_number, date_first_available, manufacturer, asin, country_of_origin)
+
+
+data = sanatizeInfoDump("""{
+    "productName": "Trojan Bareskin Condoms, Everythin Variety Pack, 10 Count",
+    "productCostWhole": "9.",
+    "productCostFraction": "78",
+    "listProducts": "Product Dimensions\n                                    \u200f\n
+              :\n                                    \u200e\n                                 3 x 2 x 5 inches; 1.45 Ounces            Item model number\n                                    \u200f\n
+                :\n                                    \u200e\n                                 10022600001543            Date First Available\n                                    \u200f\n
+      :\n                                    \u200e\n                                 March 25, 2022
+                         Manufacturer\n                                    \u200f\n
+            :\n                                    \u200e\n                                 Church & Dwight            ASIN\n                                    \u200f\n                                        :\n
+                          \u200e\n                                 B0B6YNSJB4            Country of Origin\n                                    \u200f\n                                        :\n
+         \u200e\n                                 USA",
+    "productReviewCount": "894 ratings",
+    "productReviewRating": "4.6 out of 5 stars"
+} """)
+
+data = getImportantDetails(data)
+
+
+def preprocess_data(df, vectorizer=None, fit_vectorizer=True):
+    """
+    Applies all transformations to the dataframe `df`.
+
+    Args:
+    - df: pandas DataFrame, input data to transform.
+    - vectorizer: CountVectorizer instance, if None a new one will be created and fit.
+    - fit_vectorizer: bool, whether to fit the CountVectorizer or not (useful at inference).
+
+    Returns:
+    - df: transformed pandas DataFrame.
+    - vectorizer: fitted CountVectorizer instance.
+    """
+
+    # Fit and transform the product names
+    if vectorizer is None:
+        vectorizer = CountVectorizer(binary=True)
+
+    if fit_vectorizer:
+        X = vectorizer.fit_transform(df['Product Name']).toarray()
+    else:
+        X = vectorizer.transform(df['Product Name']).toarray()
+
+    # Create a DataFrame with the one-hot encoded features
+    df_one_hot = pd.DataFrame(X, columns=vectorizer.get_feature_names_out(), index=df.index)
+    # df = df.drop(['Product Name'])
+    # Concatenate the original DataFrame with the new one-hot encoded DataFrame
+    df = pd.concat([df, df_one_hot], axis=1)
+    df.fillna(0, inplace=True)
+    df['Unit Count'] = df['Unit Count'].replace(0, 1)  # Replace 0 units with 1 to avoid division by zero
+    df['true_price'] = df['Price'] / df['Unit Count']
+    df['Keyword'] = df['Keyword'].replace(
+        {'men': 0, 'women': 1, 'missing': -1})  # Assuming 'missing' as -1 or another category
+
+    # One-hot encode 'Category'
+    one_hot_encoded = pd.get_dummies(df['Category'], prefix='Category')
+    df = df.join(one_hot_encoded)
+
+    # Replace spaces with underscores in column names and sort columns
+    df.columns = [col.replace(' ', '_') for col in df.columns]
+    df = df.sort_index(axis=1)
+
+    df = pd.read_csv('../data/amazon_products_via_rainforest_api.csv')
+    df2 = pd.read_csv('../data/amazon_products_via_rainforest_api2.csv')
+    df3 = pd.read_csv('../data/amazon_products_via_rainforest_api3.csv')
+    df4 = pd.read_csv('../data/amazon_products_via_rainforest_api4.csv')
+    df5 = pd.read_csv('../data/amazon_products_via_rainforest_api5.csv')
+    df6 = pd.read_csv('../data/amazon_products_via_rainforest_api6.csv')
+    df7 = pd.read_csv('../data/amazon_products_via_rainforest_api7.csv')
+    df5['Price'] = df5['Price'].str.replace('$', '').astype(float)
+    df6['Price'] = df6['Price'].str.replace('$', '').astype(float)
+    df7['Price'] = df7['Price'].str.replace('$', '').astype(float)
+
+    # Display the first few rows of the dataframe
+    df = pd.concat([df, df2, df3, df4, df5, df6, df7], axis=0)
+
+    # Example usage during training
+    df, vectorizer = preprocess_data(df)
+    df = df.drop(['Product_Name', 'ASIN', 'Category'], axis=1)
+    print(df.columns.to_list())
+    # Save the vectorizer for later use during inference
+    joblib.dump(vectorizer, 'vectorizer.joblib')
+    return df, vectorizer
